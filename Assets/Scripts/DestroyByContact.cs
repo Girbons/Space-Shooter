d65ff23a4d0e041caf42cc23796a6776a94440e1
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-12-18 11:27:42 +0100

    Basic Space Shooter

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
 
+	void Start() {
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null) {
+			Debug.Log("Cannot find 'GameController' script");
+		}
+
+	}
 	void OnTriggerEnter(Collider other) {
 		if (!other.CompareTag("Boundary")) {
 			if (other.CompareTag("Player")) {
 				Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+				gameController.GameOver();
 			}
+			gameController.AddScore(scoreValue);
 			Instantiate(explosion, transform.position, transform.rotation);
 			Destroy(other.gameObject);
 			Destroy(gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
@@ -9,6 +11,13 @@ public class GameController : MonoBehaviour {
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
 
 	IEnumerator SpanWaves() {
 		yield return new WaitForSeconds(startWait);
@@ -24,10 +33,44 @@ public class GameController : MonoBehaviour {
 				yield return new WaitForSeconds(spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver) {
+				restartText.text = "Press 'R' to restart";
+				restart = true;
+				break;
+			}
 		}
 	}
 
+	void UpdateScore() {
+		scoreText.text = "Score: " + score;
+	}
+
+	public void AddScore(int scoreValue) {
+		score += scoreValue;
+		UpdateScore();
+	}
+
+	public void GameOver() {
+		gameOverText.text = "GAME OVER!";
+		gameOver = true;
+	}
+
 	void Start() {
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore();
 		StartCoroutine(SpanWaves());
+	}
+
+	void Update() {
+		if (restart) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+		}
 	}
 }
